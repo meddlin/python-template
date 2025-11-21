666c4b1014bed4ddeaf3e785a5a4e7aa4ef678c2
Darrien Rushing <drushing.dev@gmail.com>
2025-11-20 22:04:27 -0600

    Adding default argparse and subparser

--- src/main_module/main.py
+++ src/main_module/main.py
@@ -1,8 +1,19 @@
 """Main module
 """
 
+import argparse
+
 def main():
     print("Python template - main()")
+
+    parser = argparse.ArgumentParser(description="Template CLI")
+    subparsers = parser.add_subparsers(dest="command")
+    list_parser = subparsers.add_parser("list", help="List data")
+
+    args = parser.parse_args()
+
+    if args.command == "list":
+        print("list action goes here")
 
 if __name__ == "__main__":
     main()
