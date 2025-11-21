9aa7a6c1864f33e9607a339b7b90b9b1b5fa24ab
Darrien Rushing <drushing.dev@gmail.com>
2025-11-20 23:05:48 -0600

    a few comments and notes update

--- src/main_module/main.py
+++ src/main_module/main.py
@@ -6,10 +6,12 @@ import argparse
 def main():
     print("Python template - main()")
 
+    # Setup parsers
     parser = argparse.ArgumentParser(description="Template CLI")
     subparsers = parser.add_subparsers(dest="command")
     list_parser = subparsers.add_parser("list", help="List data")
 
+    # Parse user input
     args = parser.parse_args()
 
     if args.command == "list":
